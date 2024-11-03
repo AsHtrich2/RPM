03f8d9d869f0f82f1838fc6c9bc06ca55406437e
AsHtrich2 <ashforbackuponly@gmail.com>
2024-11-03 18:17:00 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,5 @@ curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt update
 sudo apt install -y nodejs
-sudo apt insatll -y npm
+sudo apt install -y npm
 sudo apt install -g pm2
