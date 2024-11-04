62077f60353ccb65db40fba7cf72ab2ed315fab0
AsHtrich2 <ashforbackuponly@gmail.com>
2024-11-04 23:07:25 +0530

    Test 5

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Specify the port your application is running on
 PORT=3000
 
-# Find and kill the process running on the specified port
 PID=$(lsof -t -i :$PORT)
 
-# Check if a process was found, then kill it
 if [ -n "$PID" ]; then
     echo "Stopping application running on port $PORT (PID: $PID)"
     kill -9 $PID
