0f0c808f1abdfc1fb72a6a22c472f689d6f1a4ce
AsHtrich2 <ashforbackuponly@gmail.com>
2024-11-04 09:06:53 +0530

    Updated 9 files

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div className='flex flex-col mt-2 w-screen'>
-      <h1 className='font-bold ml-8 underline text-7xl'>Hello Bitches!</h1>
-      {/* <h1 className='font-bold ml-[1480px] underline text-7xl mt-[730px]'>Bye Bitches!</h1> */}
+      {/* <h1 className='font-bold ml-8 underline text-7xl'>Hello Bitches!</h1> */}
+      <h1 className='font-bold ml-[1480px] underline text-7xl mt-[730px]'>Bye Bitches!</h1>
     </div>
   )
 }
