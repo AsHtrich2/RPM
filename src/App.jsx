51b7f7499a31bf2b11cc1252fa387ef5c9feefab
AsHtrich2 <ashforbackuponly@gmail.com>
2024-11-04 08:51:30 +0530

    Updated 9 files

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className='flex flex-col mt-2 w-screen'>
       <h1 className='font-bold ml-8 underline text-7xl'>Hello Bitches!</h1>
-      <h1 className='font-bold ml-[1480px] underline text-7xl mt-[730px]'>Bye Bitches!</h1>
+      {/* <h1 className='font-bold ml-[1480px] underline text-7xl mt-[730px]'>Bye Bitches!</h1> */}
     </div>
   )
 }
